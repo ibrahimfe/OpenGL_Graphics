2e9e1c39673c3a90899443bfe053a5a611dc2268
ibrahimfe <ibrahiimm.f@gmail.com>
2024-03-17 20:16:15 +0700

    initial commit

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,175 +1,250 @@
-#include <glad/glad.h>
+#pragma comment(lib, "glew32s.lib")
+#define GLEW_STATIC
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
+// Window dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+GLuint VBO, VAO, shader, uniformModel;
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
-const char *fragmentShader1Source = "#version 330 core\n"
-                                    "out vec4 FragColor;\n"
-                                    "void main()\n"
-                                    "{\n"
-                                    "   FragColor = vec4(0.5f, 0.0f, 0.2f, 1.0f);\n"
-                                    "}\n\0";
-const char *fragmentShader2Source = "#version 330 core\n"
-                                    "out vec4 FragColor;\n"
-                                    "void main()\n"
-                                    "{\n"
-                                    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-                                    "}\n\0";
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.5f;
+float triIncrement = 0.01f;
+
+// rotation
+const float toRadian = 3.14159265 / 180;
+float curAngle = 0.0f;
+
+// scalling
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.9f;
+float minSize = 0.2f;
+
+// Vertex Shader code
+static const char *vShader = "                                                \n\
+#version 460                                                                  \n\
+                                                                              \n\
+layout (location = 0) in vec3 pos;											  \n\
+                                                                              \n\
+uniform mat4 model;                                                          \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    gl_Position = model * vec4(pos, 1.0);		  \n\
+}";
+
+// Fragment Shader
+static const char *fShader = "                                                \n\
+#version 460                                                                  \n\
+                                                                              \n\
+out vec4 colour;                                                               \n\
+                                                                              \n\
+void main()                                                                   \n\
+{                                                                             \n\
+    colour = vec4(1.0, 1.0, 0.0, 1.0);                                         \n\
+}";
+
+void CreateTriangle()
+{
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f};
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+}
+
+void AddShader(GLuint theProgram, const char *shaderCode, GLenum shaderType)
+{
+    GLuint theShader = glCreateShader(shaderType);
+
+    const GLchar *theCode[1];
+    theCode[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode);
+
+    glShaderSource(theShader, 1, theCode, codeLength);
+    glCompileShader(theShader);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(theShader, 1024, NULL, eLog);
+        fprintf(stderr, "Error compiling the %d shader: '%s'\n", shaderType, eLog);
+        return;
+    }
+
+    glAttachShader(theProgram, theShader);
+}
+
+void CompileShaders()
+{
+    shader = glCreateProgram();
+
+    if (!shader)
+    {
+        printf("Failed to create shader\n");
+        return;
+    }
+
+    AddShader(shader, vShader, GL_VERTEX_SHADER);
+    AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error linking program: '%s'\n", eLog);
+        return;
+    }
+
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if (!result)
+    {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error validating program: '%s'\n", eLog);
+        return;
+    }
+
+    uniformModel = glGetUniformLocation(shader, "model");
+}
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
+    // Initialise GLFW
+    if (!glfwInit())
+    {
+        printf("GLFW initialisation failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup GLFW window properties
+    // OpenGL version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core Profile
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
+    // Allow Forward Compatbility
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
+    // Create the window
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    if (!mainWindow)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        printf("GLFW window creation failed!");
         glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        return 1;
     }
 
-    // build and compile our shader program
-    // ------------------------------------
+    // Get Buffer Size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    unsigned int fragmentShaderMaroon = glCreateShader(GL_FRAGMENT_SHADER); // the first fragment shader that outputs the color maroon
-    unsigned int fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER); // the second fragment shader that outputs the color yellow
-    unsigned int shaderProgramMaroon = glCreateProgram();
-    unsigned int shaderProgramYellow = glCreateProgram(); // the second shader program
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    glShaderSource(fragmentShaderMaroon, 1, &fragmentShader1Source, NULL);
-    glCompileShader(fragmentShaderMaroon);
-    glShaderSource(fragmentShaderYellow, 1, &fragmentShader2Source, NULL);
-    glCompileShader(fragmentShaderYellow);
-    // link the first program object
-    glAttachShader(shaderProgramMaroon, vertexShader);
-    glAttachShader(shaderProgramMaroon, fragmentShaderMaroon);
-    glLinkProgram(shaderProgramMaroon);
-    // then link the second program object using a different fragment shader (but same vertex shader)
-    glAttachShader(shaderProgramYellow, vertexShader);
-    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
-    glLinkProgram(shaderProgramYellow);
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
 
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float firstTriangle[] = {
-        -0.9f, -0.5f, 0.0f, // left
-        -0.0f, -0.5f, 0.0f, // right
-        -0.45f, 0.5f, 0.0f, // top
-    };
-    float secondTriangle[] = {
-        0.0f, -0.5f, 0.0f, // left
-        0.9f, -0.5f, 0.0f, // right
-        0.45f, 0.5f, 0.0f  // top
-    };
-    unsigned int VBOs[2], VAOs[2];
-    glGenVertexArrays(2, VAOs);
-    glGenBuffers(2, VBOs);
-    // first triangle setup
-    // --------------------
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
 
-    // second triangle setup
-    // ---------------------
-    glBindVertexArray(VAOs[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]); // and a different VBO
-    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
-    glEnableVertexAttribArray(0);
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
+    if (glewInit() != GLEW_OK)
     {
-        // input
-        // -----
-        processInput(window);
+        printf("GLEW initialisation failed!");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
 
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // Setup Viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    CreateTriangle();
+    CompileShaders();
+
+    // Loop until window closed
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        // Get + Handle user input events
+        glfwPollEvents();
+
+        // Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgramMaroon);
-        // draw the first triangle using the data from our first VAO
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 3); // this call should output an maroon triangle
-        //  draw the second triangle using the data from the second VAO
-        glUseProgram(shaderProgramYellow);
-        glBindVertexArray(VAOs[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3); // this call should output a yellow triangle
+        glUseProgram(shader);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        // Mengatur transformasi model
+        glm::mat4 model(1.0f); // Matrix identitas awal
+
+        // Bergerak ke kanan
+        if (triOffset <= triMaxOffset && triOffset >= 0.0f)
+        {
+            model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        }
+        // Bergerak ke atas
+        else if (triOffset <= 2 * triMaxOffset && triOffset >= triMaxOffset)
+        {
+            model = glm::translate(model, glm::vec3(triMaxOffset, triOffset - triMaxOffset, 0.0f));
+        }
+        // Bergerak ke kiri
+        else if (triOffset <= 3 * triMaxOffset && triOffset >= 2 * triMaxOffset)
+        {
+            model = glm::translate(model, glm::vec3(3 * triMaxOffset - triOffset, triMaxOffset, 0.0f));
+        }
+        // Bergerak ke bawah
+        else if (triOffset <= 4 * triMaxOffset && triOffset >= 3 * triMaxOffset)
+        {
+            model = glm::translate(model, glm::vec3(0.0f, 4 * triMaxOffset - triOffset, 0.0f));
+        }
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+        // Gambar segitiga
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glUseProgram(0);
+
+        // Update nilai triOffset
+        triOffset += triIncrement;
+        if (triOffset >= 4 * triMaxOffset)
+            triOffset = 0.0f;
+
+        glfwSwapBuffers(mainWindow);
     }
 
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(2, VAOs);
-    glDeleteBuffers(2, VBOs);
-    glDeleteProgram(shaderProgramMaroon);
-    glDeleteProgram(shaderProgramYellow);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
     return 0;
-}
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
 }
